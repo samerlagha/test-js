c041b6b66befd9ae92641254267b33ee8f052a17
samerlagha <samer_la@mail.ru>
2019-10-26 15:08:41 +0300

    index.js correct

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let a=4;
 let b=2;
 console.log('a: ', a);
-console.log('b: значение переменной ',b);
-console.log( 'a-b = ',(a-b));
-console.log('a+b = ',(a+b));
-console.log('a/b = ',(a/b));
-console.log('a*b = ',(a*b));
+console.log('b: значение переменной '+b);
+console.log( 'a-b = '+ (a-b));
+console.log('a+b = ' + (a+b));
+console.log('a/b = ' + (a/b));
+console.log('a*b = ' + (a*b));
